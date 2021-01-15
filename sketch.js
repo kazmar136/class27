fbe444fc50f50f76d4c22cc2033b8dea56db3cd8
kazmar136 <kazmardevelopment@gmail.com>
2021-01-15 11:45:12 -0500

    sketch

--- sketch.js
+++ sketch.js
@@ -17,7 +17,7 @@ function setup(){
 
 
     ground = new Ground(600,height,1200,20);
-    platform = new Ground(150, 305, 300, 170);
+    platform = new Ground(160, 305, 300, 170);
 
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
@@ -25,7 +25,7 @@ function setup(){
     log1 = new Log(810,260,300, PI/2);
 
     box3 = new Box(700,240,70,70);
-    box4 = new Box(920,240,70,70);
+    box4 = new Box(930,240,70,70);
     pig3 = new Pig(810, 220);
 
     log3 =  new Log(810,180,300, PI/2);
@@ -34,7 +34,7 @@ function setup(){
     log4 = new Log(760,120,150, PI/7);
     log5 = new Log(870,120,150, -PI/7);
 
-    bird = new Bird(100,100);
+    bird = new Bird(110,100);
 
 }
 
